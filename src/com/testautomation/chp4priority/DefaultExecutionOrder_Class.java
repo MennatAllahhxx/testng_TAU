79433f3015c532973aa748103444c15e427841bd
Menna Hassan <h.menna85@gmail.com>
2024-08-13 10:23:01 +0300

    removed unnecessary variables

--- src/com/testautomation/chp4priority/DefaultExecutionOrder_Class.java
+++ src/com/testautomation/chp4priority/DefaultExecutionOrder_Class.java
@@ -1,9 +1,7 @@
 package com.testautomation.chp4priority;
 
-import org.openqa.selenium.WebDriver;
 import org.testng.annotations.Test;
 
 @Test
 public class DefaultExecutionOrder_Class {
-    WebDriver driver;
 }
